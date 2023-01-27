52dc7f97e607c06a9e7572e7bdddcc3767f93a66
DawidJasko <68279366+DawidJasko@users.noreply.github.com>
2023-01-27 20:40:25 +0100

    finish

--- script.js
+++ script.js
@@ -7,9 +7,9 @@ let totalImages = 0;
 let photosArray = [];
 
 // Unsplash API
-const count = 30;
+let count = 5;
 const apiKey = 'SRS22Zv4ASoNE6m3iaFmN4D0kpSIIDKFGhUa26A4I1M';
-const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+let apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
 // Check if all images were loaded
 function imageLoaded() {
@@ -18,7 +18,8 @@ function imageLoaded() {
   if (imagesLoaded === totalImages) {
     ready = true;
     loader.hidden = true;
-    console.log('ready =', ready);
+    count = 30;
+    apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
   }
 }
 
